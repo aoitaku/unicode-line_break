8e3c5cb7d69d0e174b898b7d896df7528fc750cf
aoitaku <aoitaku@gmail.com>
2015-02-25 20:00:33 +0900

    refactor db generate logic

--- generate_db.rb
+++ generate_db.rb
@@ -6,23 +6,29 @@ module Unicode
   VERSION = '7.0.0'
 end
 
-ucd = -> path do
-  Net::HTTP.get(URI.parse("http://www.unicode.org/Public/#{Unicode::VERSION}/ucd/#{path}"))
-end
-gen = -> items do
-  JSON.generate(items.each_with_object({ i: {}, r: {} }) {|(*cp, c), db|
-    cp.last ? db[:r].store(cp.first.to_i(16)..cp.last.to_i(16), c.to_sym) : db[:i].store(cp.first.to_i(16), c.to_sym)
-  })
+UCD_DIR = "http://www.unicode.org/Public/#{Unicode::VERSION}/ucd/"
+
+def ucd
+  -> path { -> pattern { -> {
+    response = Net::HTTP.get(URI.parse("#{UCD_DIR}#{path}"))
+    break unless response
+    response.scan(pattern).each_with_object({ i: {}, r: {} }) {|(*cp, c), db|
+      cp.last ?
+      db[:r].store(cp.first.to_i(16)..cp.last.to_i(16), c.to_sym) :
+      db[:i].store(cp.first.to_i(16), c.to_sym)
+    }
+  }}}
 end
 
-# line break
-File.write(
-  'ucd_linebreak.json',
-  gen[ucd['LineBreak.txt'].scan(/^([0-9A-F]+)(?:\.\.([0-9A-F]+))?;([A-Z][A-Z0-9])/)]
-)
+def dump_dbs(dbs)
+  dbs.each do |file, db|
+    JSON.dump(db[], File.open("#{file.to_s}.json", 'w+:utf-8'))
+  end
+end
 
-# east asian width
-File.write(
-  'ucd_east_asian_width.json',
-  gen[ucd['EastAsianWidth.txt'].scan(/^([0-9A-F]+)(?:\.\.([0-9A-F]+))?;([AFHNW]|Na)/)]
+dump_dbs(
+  ucd_linebreak:
+    ucd['LineBreak.txt'][/^([0-9A-F]+)(?:\.\.([0-9A-F]+))?;([A-Z][A-Z0-9])/],
+  ucd_east_asian_width:
+    ucd['EastAsianWidth.txt'][/^([0-9A-F]+)(?:\.\.([0-9A-F]+))?;([AFHNW]|Na)/]
 )
