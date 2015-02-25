d86bb3d118b5d5f96c402fd5ad8797a111a83bc3
aoitaku <aoitaku@gmail.com>
2015-02-25 18:26:58 +0900

    add implementation for east asian width db

--- generate_db.rb
+++ generate_db.rb
@@ -6,14 +6,23 @@ module Unicode
   VERSION = '7.0.0'
 end
 
-url = URI.parse("http://www.unicode.org/Public/#{Unicode::VERSION}/ucd/LineBreak.txt")
-linebreak = Net::HTTP.get(url)
-db = { i: {}, r: {} }
-linebreak.scan(/^([0-9A-F]+)(?:\.\.([0-9A-F]+))?;([A-Z][A-Z0-9])/) do |(*cp, c)|
-  if cp.last
-    db[:r].store(cp.first.to_i(16)..cp.last.to_i(16), c.to_sym)
-  else
-    db[:i].store(cp.first.to_i(16), c.to_sym)
-  end
+ucd = -> path do
+  Net::HTTP.get(URI.parse("http://www.unicode.org/Public/#{Unicode::VERSION}/ucd/#{path}"))
 end
-File.write('ucd_db.json', JSON.generate(db))
+gen = -> items do
+  JSON.generate(items.each_with_object({ i: {}, r: {} }) {|(*cp, c), db|
+    db[:r].store((cp.last ? cp.first.to_i(16)..cp.last.to_i(16) : cp.first.to_i(16)), c.to_sym)
+  })
+end
+
+# line break
+File.write(
+  'ucd_line_break.json',
+  gen[ucd['LineBreak.txt'].scan(/^([0-9A-F]+)(?:\.\.([0-9A-F]+))?;([A-Z][A-Z0-9])/)]
+)
+
+# east asian width
+File.write(
+  'ucd_east_asian_width.json',
+  gen[ucd['EastAsianWidth.txt'].scan(/^([0-9A-F]+)(?:\.\.([0-9A-F]+))?;([AFHNW]|Na)/)]
+)
